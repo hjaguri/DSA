5995690bfefb26208f142dc3739d8b8aca940714
Hansraj Jaguri <hjaguri27@gmail.com>
2023-09-15 23:33:45 +0530

    We can add some comment

--- src/Main.java
+++ src/Main.java
@@ -19,13 +19,13 @@ class Queue{
         arr=new int[x];
     }
 
-    public void push(int x){
+    public void push(int x){ // This is a push operation in queue
 
         rear++;
         arr[rear]=x;
     }
 
-    public void pop(){
+    public void pop(){ //delete the element from queue
         if(rear==-1){
             System.out.println("Empty");
         }
@@ -35,7 +35,7 @@ class Queue{
         rear--;
     }
 
-    public void printQueue(){
+    public void printQueue(){ // print the queue
         for(int i=0;i<=rear;i++){
             System.out.print(arr[i] + " ");
         }
