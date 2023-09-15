2db160ba1b8afdde8f56f5fd090da6f36135f09a
Hansraj Jaguri <hjaguri27@gmail.com>
2023-09-15 23:30:42 +0530

    We can write a code

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,44 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+       Queue q=new Queue(5);
+       q.push(2);
+        q.push(10);
+        q.printQueue();
+        System.out.println();
+        q.pop();
+        q.printQueue();
     }
+}
+
+class Queue{
+    int size;
+    int arr[];
+    int rear=-1;
+    public Queue(int x){
+        this.size=x;
+        arr=new int[x];
+    }
+
+    public void push(int x){
+
+        rear++;
+        arr[rear]=x;
+    }
+
+    public void pop(){
+        if(rear==-1){
+            System.out.println("Empty");
+        }
+        for(int i=0;i<rear;i++){
+            arr[i]=arr[i+1];
+        }
+        rear--;
+    }
+
+    public void printQueue(){
+        for(int i=0;i<=rear;i++){
+            System.out.print(arr[i] + " ");
+        }
+    }
+
 }
